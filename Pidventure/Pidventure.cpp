e1058635a28708c2dd318c8c4b130be786ef1ff2
Magnus Runesson <magnus@pokewhat.com>
2016-09-01 22:04:44 +0200

    Added experimental screen blitting

--- Pidventure/Pidventure.cpp
+++ Pidventure/Pidventure.cpp
@@ -6,16 +6,40 @@
 //  Copyright © 2016 Magnus Runesson. All rights reserved.
 //
 
+#include "Engine/Graphics/Screen.h"
 #include "Pidventure.h"
+
+float t;
 
 void game_setup()
 {
-	
+	t = 0.0f;
 }
 
 void game_loop()
 {
+	float dt = 1.0f / 60.0f;
+	t += dt * 0.5f;
+	if( t > 2.0f )
+		t -= 2.0f;
 	
+	float g = t;
+	if(g > 1.0f )
+		g = 2.0f-g;
+	
+	int x, y;
+	for(y=0; y<SCREEN_HEIGHT; y++)
+	{
+		for(x=0; x<SCREEN_WIDTH; x++)
+		{
+			float fx = ((float)x) / ((float)SCREEN_WIDTH);
+			float fy = ((float)y) / ((float)SCREEN_HEIGHT);
+			int wrofs = (y*SCREEN_WIDTH)+x;
+			screenBuffer[wrofs*4 + 0] = fx;
+			screenBuffer[wrofs*4 + 1] = g;
+			screenBuffer[wrofs*4 + 2] = fy;
+		}
+	}
 }
 
 void game_debugTrigger(int _id)
