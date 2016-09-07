2155ec2655cd5097fff073c89f7653f705d6ae1f
Magnus Runesson <magnus@pokewhat.com>
2016-09-07 06:59:07 +0200

    Test code for drawing an image

--- Pidventure/Pidventure.cpp
+++ Pidventure/Pidventure.cpp
@@ -8,17 +8,22 @@
 
 #include "Engine/Core/Debug.h"
 #include "Engine/Graphics/Screen.h"
+#include "Engine/Graphics/Image.h"
 #include "Engine/IO/Joypad.h"
 #include "Pidventure/Pidventure.h"
 
 float t;
 
 CPlayer* pPlayer;
+Image* pTestImage;
 
 void game_setup()
 {
 	pPlayer = new CPlayer();
 
+	pTestImage = imageLoad("herotest.pei");
+	debugLog("pTestImage=0x%016llx\n", pTestImage);
+	debugLog("    pixels=0x%016llx\n", pTestImage->pixels);
 	t = 0.0f;
 }
 
@@ -47,6 +52,26 @@ void game_loop()
 			screenBuffer[wrofs*4 + 0] = fx;
 			screenBuffer[wrofs*4 + 1] = g;
 			screenBuffer[wrofs*4 + 2] = fy;
+		}
+	}
+	
+	int basex = 10;
+	int basey = 10;
+	for(y=0; y<pTestImage->h; y++)
+	{
+		for(x=0; x<pTestImage->w; x++)
+		{
+			int readofs = ((y*pTestImage->w) + x) * 4;
+			int writeOfs = (((basey+y)*SCREEN_WIDTH) + (basex+x)) * 4;
+			float r = pTestImage->pixels[ readofs+0 ];
+			float g = pTestImage->pixels[ readofs+1 ];
+			float b = pTestImage->pixels[ readofs+2 ];
+			float a = pTestImage->pixels[ readofs+3 ];
+			float ia = 1.0f-a;
+			screenBuffer[ writeOfs+0 ] = (r*a)+(screenBuffer[ writeOfs+0 ]*ia);
+			screenBuffer[ writeOfs+1 ] = (g*a)+(screenBuffer[ writeOfs+1 ]*ia);
+			screenBuffer[ writeOfs+2 ] = (b*a)+(screenBuffer[ writeOfs+2 ]*ia);
+			//screenBuffer[ writeOfs+3 ] = (a;
 		}
 	}
 	
