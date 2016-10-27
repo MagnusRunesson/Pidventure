f413748576144018048933325d72548ff0da1ec4
Magnus Runesson <magnus@pokewhat.com>
2016-10-27 07:35:45 +0200

    Started refactored the world data so it can be easily loaded and unloaded, so it is easier to switch between worlds.

--- Pidventure/Content/World.cpp
+++ Pidventure/Content/World.cpp
@@ -16,31 +16,46 @@
 #include "Pidventure/Player/Player.h"
 #include "Pidventure/Player/PlayerAvatar.h"
 
+class CWorldData
+{
+public:
+	class CWorldDoorData
+	{
+	public:
+		int worldX;
+		int worldY;
+	};
+	
+	const char* pszMainScene;
+	const char* apszScene[10];
+	int numDoors;
+	CWorldDoorData aDoors[10];	// Door x always lead to apszScene[x]
+};
+
 static CDoor* g_pDoor[10];
-static CScene* g_pScene;
-static CScene* g_pScene2;
-static CScene* g_pScene3;
+static CScene* g_pMainScene;
+static CScene* g_apSubScene[10];
 static int g_numDoors;
 
 extern CPlayer* pPlayer;
 
 void worldInit()
 {
-	g_pScene = new CScene();
-	g_pScene->Load( "scene_highlands_home" );
-	g_pScene->SetSort( -1.2f );
+	g_pMainScene = new CScene();
+	g_pMainScene->Load( "scene_highlands_home" );
+	g_pMainScene->SetSort( -1.2f );
 	
-	cameraSetBounds( g_pScene );
+	cameraSetBounds( g_pMainScene );
 	
-	g_pScene2 = new CScene();
-	g_pScene2->Load( "scene_highlands_interior_test" );
-	g_pScene2->SetWorldPosition( 8*4, 8*4 );
-	g_pScene2->SetSort( -1.1f );
+	g_apSubScene[ 0 ] = new CScene();
+	g_apSubScene[ 0 ]->Load( "scene_highlands_interior_test" );
+	g_apSubScene[ 0 ]->SetWorldPosition( 8*4, 8*4 );
+	g_apSubScene[ 0 ]->SetSort( -1.1f );
 	
-	g_pScene3 = new CScene();
-	g_pScene3->Load( "scene_highlands_home_interior_townhouse" );
-	g_pScene3->SetWorldPosition( 40*4, 1*4 );
-	g_pScene3->SetSort( -1.1f );
+	g_apSubScene[ 1 ] = new CScene();
+	g_apSubScene[ 1 ]->Load( "scene_highlands_home_interior_townhouse" );
+	g_apSubScene[ 1 ]->SetWorldPosition( 40*4, 1*4 );
+	g_apSubScene[ 1 ]->SetSort( -1.1f );
 	
 	debugLog("Gamesetup start 6\n");
 	
@@ -49,34 +64,34 @@ void worldInit()
 	
 	debugLog("DoorManager init done\n");
 	
-	g_pDoor[0] = doorManager.CreateDoor( 46, 52 );
-	g_pDoor[0]->m_pSceneInside = g_pScene2;
-	g_pDoor[0]->m_pSceneOutside = g_pScene;
+	g_pDoor[ 0 ] = doorManager.CreateDoor( 46, 52 );
+	g_pDoor[ 0 ]->m_pSceneInside = g_apSubScene[ 0 ];
+	g_pDoor[ 0 ]->m_pSceneOutside = g_pMainScene;
 	
-	g_pDoor[1] = doorManager.CreateDoor( (46*4)+2, 13*4 );
-	g_pDoor[1]->m_pSceneInside = g_pScene3;
-	g_pDoor[1]->m_pSceneOutside = g_pScene;
+	g_pDoor[ 1 ] = doorManager.CreateDoor( (46*4)+2, 13*4 );
+	g_pDoor[ 1 ]->m_pSceneInside = g_apSubScene[ 1 ];
+	g_pDoor[ 1 ]->m_pSceneOutside = g_pMainScene;
 	
 	g_numDoors = 2;
 	
-	physInit( g_pScene );
+	physInit( g_pMainScene );
 }
 
 void worldUpdate()
 {
-	if( pPlayer->m_pAvatar->m_worldX < g_pScene->GetWorldLeft() + 2.0f ) debugLog( "OUtside left!\n" );
-	if( pPlayer->m_pAvatar->m_worldX > g_pScene->GetWorldRight() - 2.0f ) debugLog( "OUtside right!\n" );
+	if( pPlayer->m_pAvatar->m_worldX < g_pMainScene->GetWorldLeft() + 2.0f ) debugLog( "OUtside left!\n" );
+	if( pPlayer->m_pAvatar->m_worldX > g_pMainScene->GetWorldRight() - 2.0f ) debugLog( "OUtside right!\n" );
 	
-	g_pScene->SetViewportTopLeft((int)cameraWorldX(), (int)cameraWorldY());
-	g_pScene->Render();
+	g_pMainScene->SetViewportTopLeft((int)cameraWorldX(), (int)cameraWorldY());
+	g_pMainScene->Render();
 
 	int i;
 	for( i=0; i<g_numDoors; i++ )
 	{
-		if(g_pDoor[i]->IsOpen())
+		if( g_pDoor[ i ]->IsOpen())
 		{
-			g_pDoor[i]->m_pSceneInside->SetViewportTopLeft((int)cameraWorldX(), (int)cameraWorldY());
-			g_pDoor[i]->m_pSceneInside->Render();
+			g_pDoor[ i ]->m_pSceneInside->SetViewportTopLeft((int)cameraWorldX(), (int)cameraWorldY());
+			g_pDoor[ i ]->m_pSceneInside->Render();
 		}
 	}
 }
