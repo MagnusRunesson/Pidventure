15140c8d4cc974a5598510ff3cea3ea4e76e6aa6
Magnus Runesson <magnus@pokewhat.com>
2016-11-01 17:25:08 +0100

    Added the possibility to enable/disable the memory tracking so it isn't always enabled

--- Engine/Core/Memory.cpp
+++ Engine/Core/Memory.cpp
@@ -1,3 +1,5 @@
+#define MEMORY_TRACKING	(0)
+
 //
 //  Memory.cpp
 //  pidventure
@@ -14,21 +16,13 @@
 #undef new
 #undef delete
 
+#if MEMORY_TRACKING == 1
+
 static std::__thread_id trackedThread;
 static FILE* g_fileNew;
 static FILE* g_fileDelete;
 
-void memTrackAlloc(void* _ptr, size_t _size, const char* _pszFileName, int _line)
-{
-	fprintf( g_fileNew, "0x%016llx,%s,%i,%u\n", (unsigned long long)_ptr, _pszFileName, _line, (unsigned int)_size );
-}
-
-void memTrackDelete(void* _ptr)
-{
-	fprintf( g_fileDelete, "0x%016llx\n", (unsigned long long)_ptr );
-}
-
-void memInit()
+void memTrackInit()
 {
 	trackedThread = std::this_thread::get_id();
 	
@@ -36,74 +30,84 @@ void memInit()
 	g_fileDelete = fopen("memtrack_del.txt", "w");
 }
 
-void memExit()
+void memTrackExit()
 {
 	fclose(g_fileNew);
 	fclose(g_fileDelete);
 }
 
+void memTrackAlloc(void* _ptr, size_t _size, const char* _pszFileName, int _line)
+{
+	if(trackedThread == std::this_thread::get_id())
+	{
+		fprintf( g_fileNew, "0x%016llx,%s,%i,%u\n", (unsigned long long)_ptr, _pszFileName, _line, (unsigned int)_size );
+		//debugLog("------------------> Calling new from %s:%i - returning 0x%08x\n", _pszFileName, 0, _ptr );
+	}
+}
+
+void memTrackDelete(void* _ptr)
+{
+	if(trackedThread == std::this_thread::get_id())
+	{
+		fprintf( g_fileDelete, "0x%016llx\n", (unsigned long long)_ptr );
+	}
+}
+#else
+
+#define memTrackInit(x)
+#define memTrackExit(x)
+#define memTrackAlloc
+#define memTrackDelete
+
+#endif
+
+
+void memInit()
+{
+	memTrackInit();
+}
+
+void memExit()
+{
+	memTrackExit();
+}
+
 void* operator new(size_t size)
 {
 	void* ptr = malloc(size);
-	if(trackedThread == std::this_thread::get_id())
-	{
-		memTrackAlloc(ptr, size, "<unknown>", 0);
-		debugLog("------------------> Calling new from %s:%i - returning 0x%08x\n", "<unknown>", 0, ptr );
-	}
+	memTrackAlloc(ptr, size, "<unknown>", 0);
 	return ptr;
 }
 
 void* operator new[](size_t size)
 {
 	void* ptr = malloc(size);
-	debugLog("Main thread=%i, this thread=%i\n", trackedThread, std::this_thread::get_id());
-	
-	if(trackedThread == std::this_thread::get_id())
-	{
-		memTrackAlloc(ptr, size, "<unknown>", 0);
-		debugLog("------------------> Calling new[] from %s:%i - returning 0x%08x\n", "<unknown>", 0, ptr );
-	}
+	memTrackAlloc(ptr, size, "<unknown>", 0);
 	return ptr;
 }
 
 void* operator new(size_t size, const char* _pszFile, int _line)
 {
 	void* ptr = malloc(size);
-	if(trackedThread == std::this_thread::get_id())
-	{
-		memTrackAlloc(ptr, size, _pszFile, _line);
-		debugLog("------------------> Calling new from %s:%i - returning 0x%08x\n", _pszFile, _line, ptr );
-	}
+	memTrackAlloc(ptr, size, _pszFile, _line);
 	return ptr;
 }
 
 void* operator new[](size_t size, const char* _pszFile, int _line)
 {
 	void* ptr = malloc(size);
-	if(trackedThread == std::this_thread::get_id())
-	{
-		memTrackAlloc(ptr, size, _pszFile, _line);
-		debugLog("------------------> Calling new[] from %s:%i - returning 0x%08x\n", _pszFile, _line, ptr );
-	}
+	memTrackAlloc(ptr, size, _pszFile, _line);
 	return ptr;
 }
 
 void operator delete(void* ptr) throw()
 {
-	if(trackedThread == std::this_thread::get_id())
-	{
-		memTrackDelete(ptr);
-		debugLog("------------------> Calling delete on 0x%08x\n", ptr );
-	}
+	memTrackDelete(ptr);
 	free(ptr);
 }
 
 void operator delete[](void* ptr) throw()
 {
-	if(trackedThread == std::this_thread::get_id())
-	{
-		memTrackDelete(ptr);
-		debugLog("------------------> Calling delete[] on 0x%08x\n", ptr );
-	}
+	memTrackDelete(ptr);
 	free(ptr);
 }
