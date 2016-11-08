da7086d582b3e6d50e15e8b13f2fb3cb465441ba
Magnus Runesson <magnus@pokewhat.com>
2016-11-08 07:21:48 +0100

    Added support to leave the screen left and right to get to a new world

--- Pidventure/Content/World.cpp
+++ Pidventure/Content/World.cpp
@@ -35,10 +35,30 @@ public:
 		int worldY;
 	};
 	
+	class CExitPoint
+	{
+	public:
+		int nextRoom;
+		int nextRoomStartPoint;
+	};
+	
+	class CStartPoints
+	{
+	public:
+		int worldX;
+		int worldY;
+	};
+	
 	const char* pszMainScene;
+
+	CExitPoint exitLeft;
+	CExitPoint exitRight;
+	
 	int numSubScenes;
 	CSubScene aSubScene[10];
-	CWorldDoorData aDoor[10];	// Door x always lead to apszScene[x]
+	CWorldDoorData aDoor[10];		// Door x always lead to aSubScene[x]
+	
+	CStartPoints aStartingPoint[4];
 };
 
 static CDoor* g_pDoor[10];
@@ -50,7 +70,17 @@ extern CPlayer* pPlayer;
 
 CWorldData firstWorld {
 	"scene_highlands_home",
-	3,
+	
+	{
+		// Exit left
+		1, 1
+	},
+	{
+		// Exit right
+		2, 0
+	},
+	
+	3,	// Num doors
 	{
 		{ "scene_highlands_interior_test", 8*4, 8*4 },
 		{ "scene_highlands_home_interior_townhouse", 40*4, 1*4 },
@@ -60,11 +90,38 @@ CWorldData firstWorld {
 		{ (11*4)+2, 13*4 },
 		{ (46*4)+2, 13*4 },
 		{ ((79+7)*4)+2, (3+5)*4 },
+	},
+	{
+		{ 20, 15*4 },		// When entering from the left
+		{ 119*4, 15*4 },	// When entering from the right
+		{ 40, 62 },			// Other spawn point
+	}
+};
+
+CWorldData secondWorld {
+	"scene_highlands_firstmonster",
+	
+	// Left / right world
+	{
+		-1, -1
+	},
+	{
+		0, 0
+	},
+	
+	// Num doors
+	0,
+	{ },
+	{ },
+	{
+		{ 0, 0 },		// When entering from the left
+		{ 118*4, 11*4 },
 	}
 };
 
 CWorldData* apAllWorlds[] = {
 	&firstWorld,
+	&secondWorld,
 };
 
 CWorldData* g_pCurrentWorld;
@@ -113,6 +170,8 @@ void worldLoad(CWorldData* _pWorld)
 	}
 
 	g_numDoors = g_pCurrentWorld->numSubScenes;
+
+	physInit( &g_MainScene );
 }
 
 void worldUnload()
@@ -139,8 +198,6 @@ void worldInit()
 	
 	
 	debugLog("Gamesetup start 6\n");
-	
-	physInit( &g_MainScene );
 }
 
 void worldExit()
@@ -148,10 +205,29 @@ void worldExit()
 	worldUnload();
 }
 
+void worldSwitchTo( CWorldData::CExitPoint* _pExitPointInfo )
+{
+	worldUnload();
+	CWorldData* pNextWorld = apAllWorlds[ _pExitPointInfo->nextRoom ];
+	worldLoad( pNextWorld );
+
+	CWorldData::CStartPoints* pStartPoint = &pNextWorld->aStartingPoint[ _pExitPointInfo->nextRoomStartPoint ];
+	pPlayer->m_Avatar.SetWorldPosition( pStartPoint->worldX, pStartPoint->worldY );
+}
+
 void worldUpdate()
 {
-	if( pPlayer->m_Avatar.m_worldX < g_MainScene.GetWorldLeft() + 2.0f ) debugLog( "OUtside left!\n" );
-	if( pPlayer->m_Avatar.m_worldX > g_MainScene.GetWorldRight() - 2.0f ) debugLog( "OUtside right!\n" );
+	if( pPlayer->m_Avatar.m_worldX < g_MainScene.GetWorldLeft() + 2.0f )
+	{
+		debugLog( "OUtside left!\n" );
+		worldSwitchTo( &g_pCurrentWorld->exitLeft );
+	}
+	
+	if( pPlayer->m_Avatar.m_worldX > g_MainScene.GetWorldRight() - 2.0f )
+	{
+		debugLog( "OUtside right!\n" );
+		worldSwitchTo( &g_pCurrentWorld->exitRight );
+	}
 	
 	g_MainScene.SetViewportTopLeft((int)cameraWorldX(), (int)cameraWorldY());
 	g_MainScene.Render();
