dad82ab44e5d3761cab8c3ba15c4a4e6fbe81aab
Magnus Runesson <magnus@pokewhat.com>
2017-08-31 07:07:19 +0200

    Added splash screen data to the project

---
+++ Engine/Datawork/jrazlogo.h
@@ -0,0 +1,6 @@
+#ifndef jrazlogo_data_h
+#define jrazlogo_data_h
+
+extern "C" const Image jrazlogo;
+
+#endif // jrazlogo_data_h
---
+++ Engine/Graphics/Splash.h
@@ -0,0 +1,7 @@
+#ifndef __splash_h
+#define __splash_h
+
+void splashRender();
+
+
+#endif // __splash_h
