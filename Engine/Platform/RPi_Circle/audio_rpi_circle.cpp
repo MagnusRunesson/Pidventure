4bec99e46263c44c7ad69085cc0db81469bdae33
Magnus Runesson <magnus@pokewhat.com>
2018-01-02 20:37:15 +0100

    Changed from spaces to tabs

--- Engine/Platform/RPi_Circle/audio_rpi_circle.cpp
+++ Engine/Platform/RPi_Circle/audio_rpi_circle.cpp
@@ -4,9 +4,9 @@
 #define SAMPLE_RATE     44100
 
 CAudioRPiCircle::CAudioRPiCircle( CInterruptSystem *pInterrupt )
-:  CPWMSoundBaseDevice (pInterrupt, SAMPLE_RATE )
+: CPWMSoundBaseDevice (pInterrupt, SAMPLE_RATE )
 {
-   assert ((1 << 12) <= GetRange () && GetRange () < (1 << 13));  // 12 bit range
+	assert ((1 << 12) <= GetRange () && GetRange () < (1 << 13));  // 12 bit range
 }
 
 CAudioRPiCircle::~CAudioRPiCircle( void )
@@ -15,23 +15,23 @@ CAudioRPiCircle::~CAudioRPiCircle( void )
 
 unsigned CAudioRPiCircle::GetChunk( u32 *pBuffer, unsigned nChunkSize )
 {
-   float range = 2047.0f;
+	float range = 2047.0f;
 	
-   uint32 sample = 0;
-   while( sample < nChunkSize )
-   {
-      audioMixer.outputReadPosition++;
-      if( audioMixer.outputReadPosition >= audioMixer.outputBufferSize )
-         audioMixer.outputReadPosition = 0;
+	uint32 sample = 0;
+	while( sample < nChunkSize )
+	{
+		audioMixer.outputReadPosition++;
+		if( audioMixer.outputReadPosition >= audioMixer.outputBufferSize )
+			audioMixer.outputReadPosition = 0;
 
-      float data = audioMixer.pOutputBuffer[ audioMixer.outputReadPosition ];
-      data += 1.0f;
-      data *= range;
-      u32 intdata = (u32)data;
-      pBuffer[ sample++ ] = intdata;   // Left
-      pBuffer[ sample++ ] = intdata;   // Right
-   }
+		float data = audioMixer.pOutputBuffer[ audioMixer.outputReadPosition ];
+		data += 1.0f;
+		data *= range;
+		u32 intdata = (u32)data;
+		pBuffer[ sample++ ] = intdata;   // Left
+		pBuffer[ sample++ ] = intdata;   // Right
+	}
 
-   // The entire buffer was filled, yay!
-   return nChunkSize;
+	// The entire buffer was filled, yay!
+	return nChunkSize;
 }
